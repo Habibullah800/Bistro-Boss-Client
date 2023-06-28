88f231f0e16c75336615838c3b14890302e435de
Habibullah800 <mdhabibullah800@gmail.com>
2023-06-28 20:08:59 +0600

    Slider added

---
+++ src/Layout/pages/Home/Category/Category.jsx
@@ -0,0 +1,67 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+
+import { Pagination } from "swiper";
+
+
+import slide1 from "../../../../assets/home/slide1.jpg"
+import slide2 from "../../../../assets/home/slide2.jpg"
+import slide3 from "../../../../assets/home/slide3.jpg"
+import slide4 from "../../../../assets/home/slide4.jpg"
+import slide5 from "../../../../assets/home/slide5.jpg"
+
+const Category = () => {
+    return (
+
+        <div className="my-20 w-[80%]  mx-auto">
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={false}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src={slide1} alt="" />
+                    <h3 className="text-2xl text-center font-semibold text-lime-900 -mt-24">Salad</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide2} alt="" />
+                    <h3 className="text-2xl font-semibold text-center text-lime-900 -mt-24">Pizza</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide3} alt="" />
+                    <h3 className="text-2xl font-semibold text-center text-lime-800 -mt-24">Soups</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide4} alt="" />
+                    <h3 className="text-2xl font-semibold text-center text-lime-800 -mt-24">Desert</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide5} alt="" />
+                    <h3 className="text-2xl text-center font-semibold text-lime-900 -mt-24">Salad</h3>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <img src={slide2} alt="" />
+                    <h3 className="text-2xl font-semibold text-center text-lime-900 -mt-24">Pizza</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide3} alt="" />
+                    <h3 className="text-2xl font-semibold text-center text-lime-800 -mt-24">Soups</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide4} alt="" />
+                    <h3 className="text-2xl font-semibold text-center text-lime-800 -mt-20">Desert</h3>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Category;
