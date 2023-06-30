b3189af87cd42ab62d93d29e1c01c9f192e5df2e
Habibullah800 <mdhabibullah800@gmail.com>
2023-06-30 23:06:56 +0600

    shared covered component added

--- src/Layout/pages/Menu/Menu/Menu.jsx
+++ src/Layout/pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
-
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from "../../../../assets/menu/banner3.jpg"
 
 const Menu = () => {
     return (
@@ -7,7 +8,7 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro | Menu</title>
             </Helmet>
-            Menu here
+            <Cover img={menuImg} title="our menu"></Cover>
         </div>
     );
 };
